7ecc2b6d3550811f744ef7e6f21f94a091027697
JoJaWiley <joshua.wiley@summitoic.edu>
2023-08-23 18:26:22 -0500

    first commit

---
+++ src/main/java/com/example/sqch10ex4/Sqch10ex4Application.java
@@ -0,0 +1,13 @@
+package com.example.sqch10ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sqch10ex4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sqch10ex4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqch10ex4/Sqch10ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqch10ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sqch10ex4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
